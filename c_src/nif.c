72ee316316499a0ceb4235f42cf66abc273b34ec
zhaohansprt <2463885916@qq.com>
2017-03-01 15:04:53 +0800

    Revert "Update nif.c"

--- c_src/nif.c
+++ c_src/nif.c
@@ -26,12 +26,6 @@
 %% THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#ifdef _WIN32  
-#   define WIN32_LEAN_AND_MEAN  
-#   include <Windows.h>  
-#   pragma comment(lib, "erts_MD.lib")  
-#endif  
-
 #include <sys/types.h>
 #include <string.h>
 #include <unistd.h>
@@ -51,41 +45,6 @@
 #include "uthash.h"
 
 #include "erl_nif.h"
-
-
-
-#define BILLION                             (1E9)
-
-static BOOL g_first_time = 1;
-static LARGE_INTEGER g_counts_per_sec;
-
-int clock_gettime(int dummy, struct timespec *ct)
-{
-	LARGE_INTEGER count;
-
-	if (g_first_time)
-	{
-		g_first_time = 0;
-
-		if (0 == QueryPerformanceFrequency(&g_counts_per_sec))
-		{
-			g_counts_per_sec.QuadPart = 0;
-		}
-	}
-
-	if ((NULL == ct) || (g_counts_per_sec.QuadPart <= 0) ||
-		(0 == QueryPerformanceCounter(&count)))
-	{
-		return -1;
-	}
-
-	ct->tv_sec = count.QuadPart / g_counts_per_sec.QuadPart;
-	ct->tv_nsec = ((count.QuadPart % g_counts_per_sec.QuadPart) * BILLION) / g_counts_per_sec.QuadPart;
-
-	return 0;
-}
-
-
 
 /*
    * 2q cache made using TAILQs
@@ -235,7 +194,7 @@ clock_now(struct timespec *ts)
 	ts->tv_sec = mts.tv_sec;
 	ts->tv_nsec = mts.tv_nsec;
 #else
-	clock_gettime(1, ts);
+	clock_gettime(CLOCK_MONOTONIC, ts);
 #endif
 }
 
@@ -261,7 +220,7 @@ destroy_cache_node(struct cache_node *n)
 			nextin = TAILQ_NEXT(in, entry);
 			if (in->node == n) {
 				TAILQ_REMOVE(&(n->c->incr_head[i]), in, entry);
-				InterlockedDecrement(&(n->c->incr_count));
+				__sync_sub_and_fetch(&(n->c->incr_count), 1);
 				in->node = 0;
 				enif_free(in);
 			}
@@ -295,7 +254,7 @@ cache_bg_thread(void *arg)
 		/* sleep until there is work to do */
 		enif_cond_wait(c->check_cond, c->ctrl_lock);
 
-		InterlockedIncrement(&(c->wakeups));
+		__sync_add_and_fetch(&(c->wakeups), 1);
 		dud = 1;
 
 		/* we have to let go of ctrl_lock so we can take cache_lock then
@@ -311,7 +270,7 @@ cache_bg_thread(void *arg)
 				struct cache_incr_node *n;
 				n = TAILQ_FIRST(&(c->incr_head[i]));
 				TAILQ_REMOVE(&(c->incr_head[i]), n, entry);
-				InterlockedDecrement(&(c->incr_count));
+				__sync_sub_and_fetch(&(c->incr_count), 1);
 
 				dud = 0;
 
@@ -391,7 +350,7 @@ cache_bg_thread(void *arg)
 		}
 
 		if (dud)
-			InterlockedDecrement(&(c->dud_wakeups));
+			__sync_add_and_fetch(&(c->dud_wakeups), 1);
 		/* now let go of the cache_lock that we took right back at the start of
 		   this iteration */
 		enif_rwlock_rwunlock(c->cache_lock);
@@ -637,9 +596,9 @@ stats(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
 		enif_rwlock_rlock(c->cache_lock);
 		q1s = enif_make_uint64(env, c->q1.size);
 		q2s = enif_make_uint64(env, c->q2.size);
-		incrs = enif_make_uint64(env, InterlockedExchangeAdd(&(c->incr_count), 0));
-		wakeups = enif_make_uint64(env, InterlockedExchangeAdd(&(c->wakeups), 0));
-		duds = enif_make_uint64(env, InterlockedExchangeAdd(&(c->dud_wakeups), 0));
+		incrs = enif_make_uint64(env, __sync_fetch_and_add(&(c->incr_count), 0));
+		wakeups = enif_make_uint64(env, __sync_fetch_and_add(&(c->wakeups), 0));
+		duds = enif_make_uint64(env, __sync_fetch_and_add(&(c->dud_wakeups), 0));
 		enif_rwlock_runlock(c->cache_lock);
 		ret = enif_make_tuple7(env,
 			enif_make_uint64(env, c->hit),
@@ -760,7 +719,7 @@ get(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
 		HASH_FIND(hh, c->lookup, kbin.data, kbin.size, n);
 		if (!n) {
 			enif_rwlock_runlock(c->lookup_lock);
-			InterlockedIncrement(&c->miss);
+			__sync_add_and_fetch(&c->miss, 1);
 			enif_consume_timeslice(env, 10);
 			return enif_make_atom(env, "notfound");
 		}
@@ -769,7 +728,7 @@ get(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
 			clock_now(&now);
 			if (n->expiry.tv_sec < now.tv_sec) {
 				enif_rwlock_runlock(c->lookup_lock);
-				InterlockedIncrement(&c->miss);
+				__sync_add_and_fetch(&c->miss, 1);
 				enif_consume_timeslice(env, 10);
 				return enif_make_atom(env, "notfound");
 			}
@@ -778,14 +737,14 @@ get(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
 		in = enif_alloc(sizeof(*in));
 		memset(in, 0, sizeof(*in));
 		in->node = n;
-		InterlockedIncrement(&c->hit);
+		__sync_add_and_fetch(&c->hit, 1);
 
 		tid = enif_thread_self();
 		HASH_SFH(&tid, sizeof(ErlNifTid), N_INCR_BKT, hashv, bkt);
 		enif_mutex_lock(c->incr_lock[bkt]);
 		TAILQ_INSERT_TAIL(&(c->incr_head[bkt]), in, entry);
 		enif_mutex_unlock(c->incr_lock[bkt]);
-		incrqs = InterlockedIncrement(&(c->incr_count));
+		incrqs = __sync_add_and_fetch(&(c->incr_count), 1);
 
 		ret = enif_make_resource_binary(env, n->val, n->val, n->vsize);
 		enif_rwlock_runlock(c->lookup_lock);
